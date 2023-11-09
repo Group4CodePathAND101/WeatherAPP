624ab959051630f35737d921c0c6570090e2699e
Emanuel Warzel <44512700+cello305@users.noreply.github.com>
2023-11-09 17:05:37 -0500

    First update
    some error fixes

--- app/src/main/java/com/driuft/random_pets_starter/WeatherAdapter.kt
+++ app/src/main/java/com/driuft/random_pets_starter/WeatherAdapter.kt
@@ -7,34 +7,40 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.driuft.weatherapp.R
+import com.driuft.random_pets_starter.R
+import com.driuft.weatherapp.MainActivity.Weather
 
-class WeatherAdapter(private val weatherList: List<MainActivity.Weather>) :
+class WeatherAdapter(private val weatherList: List<Weather>) :
     RecyclerView.Adapter<WeatherAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val weatherIcon: ImageView = view.findViewById(R.id.weather_icon)
-        val weatherName: TextView = view.findViewById(R.id.weather_name)
-        val weatherStats: TextView = view.findViewById(R.id.weather_stats)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameTextView: TextView = itemView.findViewById(R.id.weather_name)
+        val temperatureTextView: TextView = itemView.findViewById(R.id.weather_temperature)
+        val humidityTextView: TextView = itemView.findViewById(R.id.weather_humidity)
+        val iconImageView: ImageView = itemView.findViewById(R.id.weather_icon)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.weather_item, parent, false)
-        return ViewHolder(view)
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        val weatherView = inflater.inflate(R.layout.item_weather, parent, false)
+
+        return ViewHolder(weatherView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val currentWeather = weatherList[position]
+        val weather = weatherList[position]
+        holder.nameTextView.text = weather.name
+        holder.temperatureTextView.text = "${weather.temperature}°C"
+        holder.humidityTextView.text = "Humidity: ${weather.humidity}%"
 
-        // Load weather icon from OpenWeatherMap icon URL
-        val iconUrl = "http://openweathermap.org/img/w/${currentWeather.icon}.png"
-        Glide.with(holder.itemView)
-            .load(iconUrl)
-            .into(holder.weatherIcon)
-
-        holder.weatherName.text = currentWeather.name
-        holder.weatherStats.text = "Temp: ${currentWeather.temperature}°C, Humidity: ${currentWeather.humidity}%"
+        // Assuming you have a valid url for the weather icon
+        Glide.with(holder.itemView.context)
+            .load("https://openweathermap.org/img/wn/${weather.icon}.png")
+            .into(holder.iconImageView)
     }
 
-    override fun getItemCount() = weatherList.size
+    override fun getItemCount(): Int {
+        return weatherList.size
+    }
 }
